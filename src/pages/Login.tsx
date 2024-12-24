64cd0ecd380b0cd65780e8b02b25d486a25a2e7c
ronyislam502 <ronyislam502@gmail.com>
2024-12-24 18:00:38 +0600

    login form

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { Row } from "antd";
+import UMForm from "../components/ui/form/UMForm";
+import UMInput from "../components/ui/form/UMInput";
+import { FieldValues } from "react-hook-form";
 
 const Login = () => {
-  return <div></div>;
+  const onSubmit = (data: FieldValues) => {
+    console.log(data);
+  };
+
+  return (
+    <Row>
+      <UMForm onSubmit={onSubmit}>
+        <UMInput type="text" name="userId" label="ID" />
+        <UMInput type="text" name="password" label="Password" />
+      </UMForm>
+    </Row>
+  );
 };
 
 export default Login;

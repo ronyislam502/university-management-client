efa137b8a0772b9246a028f40ee06887f157f33b
ronyislam502 <ronyislam502@gmail.com>
2025-01-03 00:50:27 +0600

    semester data

--- src/pages/admin/Academic Management/CreateSemester.tsx
+++ src/pages/admin/Academic Management/CreateSemester.tsx
@@ -8,6 +8,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { academicSemesterSchema } from "../../../schemas/academicManagement.schema";
 import { toast } from "sonner";
 import { useAddAcademicSemesterMutation } from "../../../redux/features/admin/academicManagementApi";
+import { TResponse } from "../../../types/global";
 
 const currentYear = new Date().getFullYear();
 const yearOptions = [0, 1, 2, 3, 4, 5, 6, 7, 8].map((number) => ({
@@ -19,6 +20,8 @@ const CreateSemester = () => {
   const [addAcademicSemester] = useAddAcademicSemesterMutation();
 
   const onSubmit = async (data: FieldValues) => {
+    const toastId = toast.loading("creating...");
+
     const name = semesterOptions[Number(data?.name) - 1]?.label;
 
     const semesterData = {
@@ -28,34 +31,57 @@ const CreateSemester = () => {
       startMonth: data?.startMonth,
       endMonth: data?.endMonth,
     };
-    console.log(semesterData);
     try {
-      const res = await addAcademicSemester(semesterData);
-      console.log(res);
+      const res = (await addAcademicSemester(semesterData)) as TResponse;
+
+      if (res?.error) {
+        toast.error(res?.error?.data?.message, { id: toastId, duration: 2000 });
+      } else {
+        toast.success(res?.data?.message, { id: toastId, duration: 2000 });
+      }
     } catch (error) {
-      toast.error("somethings is wrong");
+      toast.error("Something is wrong", { id: toastId, duration: 2000 });
     }
   };
 
   return (
-    <Flex justify="center" align="center">
-      <Col span={10}>
-        <UMForm
-          onSubmit={onSubmit}
-          resolver={zodResolver(academicSemesterSchema)}
-        >
-          <UMSelect label="Name" name="name" options={semesterOptions} />
-          <UMSelect label="Year" name="year" options={yearOptions} />
-          <UMSelect
-            label="Start Month"
-            name="startMonth"
-            options={monthOptions}
-          />
-          <UMSelect label="End Month" name="endMonth" options={monthOptions} />
-          <Button htmlType="submit">Submit</Button>
-        </UMForm>
-      </Col>
-    </Flex>
+    <div>
+      <div
+        style={{
+          justifyItems: "center",
+          alignItems: "center",
+          padding: "40px",
+          color: "blue",
+          fontSize: "20px",
+        }}
+      >
+        <h2>Create Semester Please!!!!</h2>
+      </div>
+      <div>
+        <Flex justify="center" align="center">
+          <Col span={10}>
+            <UMForm
+              onSubmit={onSubmit}
+              resolver={zodResolver(academicSemesterSchema)}
+            >
+              <UMSelect label="Name" name="name" options={semesterOptions} />
+              <UMSelect label="Year" name="year" options={yearOptions} />
+              <UMSelect
+                label="Start Month"
+                name="startMonth"
+                options={monthOptions}
+              />
+              <UMSelect
+                label="End Month"
+                name="endMonth"
+                options={monthOptions}
+              />
+              <Button htmlType="submit">Submit</Button>
+            </UMForm>
+          </Col>
+        </Flex>
+      </div>
+    </div>
   );
 };
 
--- src/redux/features/admin/academicManagementApi.ts
+++ src/redux/features/admin/academicManagementApi.ts
@@ -1,13 +1,30 @@
+import { TQueryParam, TResponseRedux } from "../../../types";
+import { TAcademicSemester } from "../../../types/academicSemester";
 import { baseApi } from "../../api/baseApi";
 
 const academicManagementApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     getAllAcademicSemesters: builder.query({
-      query: (semesterInfo) => ({
-        url: "/semesters",
-        method: "GET",
-        body: semesterInfo,
-      }),
+      query: (args) => {
+        const params = new URLSearchParams();
+
+        if (args) {
+          args.forEach((item: TQueryParam) => {
+            params.append(item.name, item.value as string);
+          });
+        }
+        return {
+          url: "/semesters",
+          method: "GET",
+          params: params,
+        };
+      },
+      transformResponse: (response: TResponseRedux<TAcademicSemester>) => {
+        return {
+          data: response.data,
+          meta: response.meta,
+        };
+      },
     }),
     addAcademicSemester: builder.mutation({
       query: (data) => ({
